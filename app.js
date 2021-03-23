224de587cb4aac75ebb7ea53f77e81f1d4942c06
unknown <soryforyou1234@gmail.com>
2021-03-23 17:52:46 +0200

    the end

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { sequelize } = require('./models')
+
+const app=express()
+app.use(express.json())
+
+async function main(){
+    await sequelize.sync({force:true})
+}
+main()
